9f2806ad647a9aefe1e01f4c01003c6c7a7beda7
Alexander Swann <alexander.swann@digital.homeoffice.gov.uk>
2023-04-29 18:59:25 +0100

    Update ASC migrations with applicant_id for saved_applications table

--- services/asc/migrations/20230428215725_saved_applications.js
+++ services/asc/migrations/20230428215725_saved_applications.js
@@ -2,6 +2,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('saved_applications', table => {
     table.increments();
+    table.string('applicant_id').notNullable();
     table.integer('recruiter_id').unsigned().notNullable().references('id').inTable('recruiters');
     table.string('email').notNullable();
     table.json('session').notNullable();
