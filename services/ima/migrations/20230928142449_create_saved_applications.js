5926c0c21daee22e786523849280a4292f11aa91
TemitopeAyokuHO <138882912+TemitopeAyokuHO@users.noreply.github.com>
2023-10-02 14:16:00 +0100

    imb-20: setup ima service in hof-rds-api (#13)
    Merged master to imb-20

---
+++ services/ima/migrations/20230928142449_create_saved_applications.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('saved_applications', table => {
+        table.increments();
+        table.string('uan').notNullable();
+        table.string('email').notNullable();
+        table.string('date_of_birth').notNullable();
+        table.json('session').notNullable();
+        table.timestamps(true, true);
+      });  
+  
+};
+
+exports.down = function(knex) {
+    return knex.schema.createTable('saved_applications');  
+};
