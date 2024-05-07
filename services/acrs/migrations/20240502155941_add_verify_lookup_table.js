4cd2b62af2652fcfa54d449d4972a3f47881502a
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2024-05-07 10:50:03 +0100

    ACRS-39 Let brp and uan columns default to NULL

--- services/acrs/migrations/20240502155941_add_verify_lookup_table.js
+++ services/acrs/migrations/20240502155941_add_verify_lookup_table.js
@@ -2,8 +2,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable('verify_lookup', table => {
     table.string('date_of_birth').notNullable();
-    table.string('brp');
-    table.string('uan');
+    table.string('brp').defaultTo(null);
+    table.string('uan').defaultTo(null);
   });
 };
 
