c080a7bf32498a504b8ad7e3639f178700ee27a5
Jamie Carter <jamie.carter@digital.homeoffice.gov.uk>
2025-03-06 16:10:33 +0000

    update column name in applicants to be more precise

--- services/csl/migrations/20250304133856_applicants.js
+++ services/csl/migrations/20250304133856_applicants.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('applicants', table => {
     table.increments('applicant_id');
     table.string('username').unique().notNullable();
-    table.string('kc_id').unique().notNullable();
+    table.string('keycloak_uuid').unique().notNullable();
     table.timestamps(true, true);
   });
 };
