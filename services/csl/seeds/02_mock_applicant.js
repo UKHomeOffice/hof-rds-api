a4581ed5a1c50dfa1b3cd16212d5ed330aab4934
Vinodha Samiyappan <vinodha.samiyappan@digital.homeoffice.gov.uk>
2025-06-09 16:42:37 +0100

    Added two new entries in applicants table in CSL service

--- services/csl/seeds/02_mock_applicant.js
+++ services/csl/seeds/02_mock_applicant.js
@@ -7,6 +7,14 @@ exports.seed = function (knex) {
         {
           applicant_id: 1,
           username: 'test-approved'
+        },
+        {
+          applicant_id: 2,
+          username: 'test-external-user'
+        },
+        {
+          applicant_id: 3,
+          username: 'test2-approved'
         }
       ]);
     });
