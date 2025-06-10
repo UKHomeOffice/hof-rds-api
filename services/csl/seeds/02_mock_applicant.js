837ea946a879fa0c9f4e6f1c035d4908aae0b33f
Vinodha Samiyappan <vinodha.samiyappan@digital.homeoffice.gov.uk>
2025-06-10 17:58:09 +0100

    Removed the logic for deleting not referenced entries in applicants

--- services/csl/seeds/02_mock_applicant.js
+++ services/csl/seeds/02_mock_applicant.js
@@ -1,25 +1,19 @@
 exports.seed = function (knex) {
-  return knex('applicants')
-    // Delete applicants not referenced in applications
-    .whereNotIn('applicant_id', knex('applications').select('applicant_id'))
-    .del()
-    .then(function () {
-      // Insert or update applicants
-      return knex('applicants').insert([
-        {
-          applicant_id: 1,
-          username: 'TEST-APPROVED'
-        },
-        {
-          applicant_id: 2,
-          username: 'TEST-EXTERNAL-USER'
-        },
-        {
-          applicant_id: 3,
-          username: 'TEST2-APPROVED'
-        }
-      ])
-        .onConflict('applicant_id')
-        .merge();
-    });
+  // Insert or update applicants
+  return knex('applicants').insert([
+    {
+      applicant_id: 1,
+      username: 'TEST-APPROVED'
+    },
+    {
+      applicant_id: 2,
+      username: 'TEST-EXTERNAL-USER'
+    },
+    {
+      applicant_id: 3,
+      username: 'TEST2-APPROVED'
+    }
+  ])
+    .onConflict('applicant_id')
+    .merge();
 };
