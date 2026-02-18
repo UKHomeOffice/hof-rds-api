118e1edf57ff74eb3450dca29091c481ebf396b9
Vivek Kumar <vivek.kumar1@digital.homeoffice.gov.uk>
2026-02-18 14:18:18 +0000

    ASCS-685 update migration logic

--- services/asc/migrations/20260203121500_add_submitted_at_column_sa.js
+++ services/asc/migrations/20260203121500_add_submitted_at_column_sa.js
@@ -1,21 +1,11 @@
 exports.up = function(knex) {
-  return knex.schema.hasColumn('saved_applications', 'submitted_at')
-    .then(exists => {
-      if (!exists) {
-        return knex.schema.table('saved_applications', table => {
-          table.timestamp('submitted_at');
-        });
-      }
+    return knex.schema.table('saved_applications', table => {
+        table.timestamp('submitted_at').defaultTo(null);
     });
 };
 
 exports.down = function(knex) {
-  return knex.schema.hasColumn('saved_applications', 'submitted_at')
-    .then(exists => {
-      if (exists) {
-        return knex.schema.table('saved_applications', table => {
-          table.dropColumn('submitted_at');
-        });
-      }
+    return knex.schema.table('saved_applications', table => {
+        table.dropColumn('submitted_at');
     });
 };
